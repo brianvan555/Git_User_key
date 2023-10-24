cb7d1113294d820af9291e912a4da4950568a3e5
Brian Liu <67460768+brianvan555@users.noreply.github.com>
2023-10-24 11:24:57 +0800

    Update github_set_global_user_detail.bat

--- github_set_global_user_detail.bat
+++ github_set_global_user_detail.bat
@@ -1,13 +1,13 @@
 @echo off
 chcp 65001
 
-:: Git 使用者名稱
-set user_name=brianvan555
-:: Git 使用者信箱
-set user_email=billy850424@gmail.com
+:: GitHub 使用者名稱
+set user_name=type your GitHub user name here
+:: GitHub 使用者信箱
+set user_email=type your GitHub email address here
 
 git config --global user.name "%user_name%"
 git config --global user.email "%user_email%"
 
-echo 設定 User Details 完成
+echo User Details setup successful
 pause
